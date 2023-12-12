a4991ee752800a8d9f06b6332d38dd86545fc4ba
makoto hata <makotohata0429@gmail.com>
2023-12-12 23:16:32 +0900

    add create todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -4,5 +4,20 @@ class TodosController < ApplicationController
   end
 
   def new
+    @todo = Todo.new
   end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to root_path
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def todo_params
+      params.require(:todo).permit(:title)
+    end
 end
